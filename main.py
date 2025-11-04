30e750b30ae044ea6da90c4c385dcf0f405e84de
wotsu <y1u0s2i7@gmail.com>
2025-11-04 09:09:48 +0900

    fix_routing

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from ai import search_restaurant
+from db import init_db, close_db
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class SearchRequest(BaseModel):
+    query: str
 
 @app.get("/api/search/")
-async def search_restaurants():
-    // ai.pyの関数ができたら書く
-    pass
+async def search_restaurants(search_request: SearchRequest):
+    conn = init_db()
+    search_result = await search_restaurant(search_request.query, conn)
+    close_db(conn)
+    return search_result
 
 
 if __name__ == "__main__":
