7838464b128ebd90af87a2df3afea399c89264b6
wotsu <y1u0s2i7@gmail.com>
2025-11-04 09:10:49 +0900

    fix_routing2

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ async def search_restaurants(search_request: SearchRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
